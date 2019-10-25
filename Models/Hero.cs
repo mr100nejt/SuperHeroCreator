85738e22516adbb4c44135ae6c7aff07e9ab1db6
jacob thompson <you@example.com>
2019-10-25 11:24:25 -0500

    changed the Hero clss

---
+++ Models/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string superHeroName { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryPower { get; set; }
+        public string secondaryPower { get; set; }
+        public string catchPhrase { get; set; }
+
+       public static List<Hero> heroList = new List<Hero>(); 
+
+
+
+    }
+}
